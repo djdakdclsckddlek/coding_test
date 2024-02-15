dbdb0ac60291ff4d98cb488d2d99ff28d6733028
KIMCHANGHWAN <138690980+Blood-donation-day@users.noreply.github.com>
2024-02-15 18:03:00 +0900

    [level 3] Title: 야근 지수, Time: 393.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,14 +1,15 @@
 import heapq as hp
 
 def solution(n, workers):
+    if n >= sum(workers):
+        return 0
     workers = [-i for i in workers]
+    
     hp.heapify(workers)
     for _ in range(n):
         work = hp.heappop(workers)
         if work < 0:
             hp.heappush(workers, work + 1)
-        else:
-            hp.heappush(workers, work)
         
     answer = sum([i**2 for i in workers])
     return answer
