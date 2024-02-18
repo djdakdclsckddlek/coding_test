7641eb9237128e289f8e8732d90208bb465ee52a
KIMCHANGHWAN <138690980+Blood-donation-day@users.noreply.github.com>
2024-02-19 06:55:28 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    stack = []
+    for n in number:
+        while stack and stack[-1] < n and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(n)
+        
+    if k!= 0:
+        stack = stack[:-k]
+    return ''.join(stack)
