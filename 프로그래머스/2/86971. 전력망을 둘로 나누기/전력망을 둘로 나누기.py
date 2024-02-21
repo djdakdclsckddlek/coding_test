dbac1773dd553a29047a7ff0c230244db6671198
KIMCHANGHWAN <138690980+Blood-donation-day@users.noreply.github.com>
2024-02-22 01:05:04 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,33 @@
-def dfs(start, graph, visited, check_link):
+def dfs(start, visited, graph, check_link):
     cnt = 1
-    visited[start] = True            # 시작 노드 방문
-    for adj_v in graph[start]:      # 시작 노드에 연결되어있는 다들 노드들
-        #연결된 노드가 방문한적 없고 
-        if visited[adj_v] == False and check_link[start][adj_v]:  
-            cnt += dfs(adj_v, graph, visited, check_link)
+    visited[start] = 1
+    
+    for adj_v in graph[start]:
+        if visited[adj_v] == 0 and check_link[start][adj_v]:
+            visited[adj_v] = 1
+            cnt += dfs(adj_v, visited, graph, check_link)
     
     return cnt
 
+    
 def solution(n, wires):
-    answer = n
-    
-    # 끊은 간선인지 아닌지 체크하는 리스트
-    check_link = [[True] * (n+1) for _ in range(n+1)]
-    # 송전탑 그래프
+    # 그래프랑 연결여부 체크하고
+    # 연결 끊어보면서 연결된 송전망 개수차이 새기
     graph = [[] for _ in range(n+1)]
-    
+    check_link = [[True] * (n+1) for _ in range(n+1)]
+    answer = 100
     for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-    
-    for a, b in wires:
-        visited = [False] * (n+1)                   # 간선 정보를 다 확인하면서
-        check_link[a][b] = False                    # a에서 b 그룹의 붙어있는 송전탑 개수를 세기위해
-        cnt_a = dfs(a, graph, visited, check_link)  # a랑 붙어있는 송전탑 개수
-        cnt_b = dfs(b, graph, visited, check_link)  # b랑 붙어있는 송전탑 개수
-        check_link[a][b] = True                     # 끊은 간선 다시 연결
+        
+    for a,b in wires:
+        visited = [0] * (n+1)
+        check_link[a][b] = False
+        cnt_a = dfs(a, visited, graph, check_link)
+        cnt_b = dfs(b, visited, graph, check_link)
+        check_link[a][b] = True
         
         answer = min(answer, abs(cnt_a - cnt_b))
     
     return answer
+        
