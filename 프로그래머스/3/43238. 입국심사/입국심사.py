8201243e74997c0a7da7db37fd8014ba1b29d794
KIMCHANGHWAN <138690980+Blood-donation-day@users.noreply.github.com>
2024-02-24 11:18:36 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,25 +1,27 @@
+# 이분탐색  왼쪽 오른쪽 오차를 줄여나가면서 찾기
 def solution(n, times):
     answer = 0
-    # 최소, 최대 시간을 설정하고 mid를 줄여나가면서 시간을 맞추기
+    
+    # 최소 최대
     left, right = 1, times[-1] * n
     
+    #오차 줄여나가기
     while left <= right:
-        mid = (left + right ) // 2
+        mid = (left + right) // 2
         people = 0
         
         for time in times:
-            # 모든 심사관이 mid분 동안 심사한 사람의 수
             people += mid // time
             
             if people >= n:
                 break
         
-        # n명 보다 같거나 많이 심사할 수 있는경우
         if people >= n:
             answer = mid
             right = mid -1
-        # n명 보다 적게 심사하는 경우
         elif people < n:
             left = mid +1
     
-    return answer
+    return left
+        
+        
