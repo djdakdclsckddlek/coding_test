ffe2ba83a543ab0df30f5f3a694cbeec55ccb862
KIMCHANGHWAN <138690980+Blood-donation-day@users.noreply.github.com>
2024-02-24 11:30:03 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,27 +1,26 @@
-# 이분탐색  왼쪽 오른쪽 오차를 줄여나가면서 찾기
 def solution(n, times):
     answer = 0
     
-    # 최소 최대
     left, right = 1, times[-1] * n
     
-    #오차 줄여나가기
+    # left right 차이가 줄어들다가
+    # 정답을 맞추면(left = right되는순간) right가 mid-1이되고
+    # 그순간 left > right가 되서 반복문을 나오고
+    # left가 정답임으로 left return 
+    
     while left <= right:
-        mid = (left + right) // 2
         people = 0
-        
+        mid = (left + right) // 2
         for time in times:
+            
             people += mid // time
             
             if people >= n:
                 break
         
         if people >= n:
-            answer = mid
             right = mid -1
         elif people < n:
-            left = mid +1
-    
+            left = mid + 1
+            
     return left
-        
-        
